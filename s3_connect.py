4f5cb0551acba7c30554cd56b6a28fa3204eb153
Miguel <michail.pliuscik@gmail.com>
2023-06-22 16:00:45 +0100

    pandas notebook

--- s3_connect.py
+++ s3_connect.py
@@ -1,6 +1,5 @@
-import boto3
+import boto3, os
 from  dotenv import load_dotenv
-import os
 
 # AWS S3 credentials
 """
@@ -12,20 +11,51 @@ export aws_secret_access_key = "your_secret_access_key"
 
 """
 
-load_dotenv()
-aws_access_key_id = os.environ["aws_access_key_id"]
-aws_secret_access_key = os.environ["aws_secret_access_key"]
+def s3_connection():
+    
+    # Loading sessetive data
+    load_dotenv("/Users/miguel/Desktop/Coding/Sparta/SpartaGlobalETLPipeline/final_project/.env")
+    
+    aws_access_key_id = os.environ["aws_access_key_id"]
+    aws_secret_access_key = os.environ["aws_secret_access_key"]
+    
+    # Creating an S3 client object
+    s3_client = boto3.client("s3", aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    return s3_client
 
 
-# S3 bucket and file details
-s3_bucket_name = "data-eng-228-final-project"
-s3_file_key = "Academy/Business_20_2019-02-11.csv"
-local_file_path = "your_directory/file_name"
+def get_all_keys():
+    
+    object_keys = []
 
+    # Parameters for pagination
+    params = {'Bucket': 'data-eng-228-final-project'}
 
-# Download file from S3
-s3_client = boto3.client("s3", aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-s3_client.download_file(s3_bucket_name, s3_file_key, local_file_path)
+    # Loop through the objects in the S3 bucket
+    while True:
+        response = s3_connection().list_objects_v2(**params)
+        
+        # Retrieve the object keys from the response
+        for obj in response['Contents']:
+            object_keys.append(obj['Key'])
 
+        # Check if there are more objects to retrieve
+        if 'NextContinuationToken' in response:
+            params['ContinuationToken'] = response['NextContinuationToken']
+        else:
+            break
+    return object_keys
+    
 
-print("Data transfer completed successfully.")
+def select_file_type(type):
+    file_list = []
+    for file in get_all_keys():
+        if file.endswith(type):
+            file_list.append(file)
+    return file_list
+
+def get_file(file_name):
+    s3_file_path = f"s3://{'data-eng-228-final-project'}/{file_name}"
+    return s3_file_path
+
+    
