b17bdbac26d2a5f8f10a282d7a7d56530fba1e4d
Gurjinder <gurjinder.bassi1@hotmail.co.uk>
2023-06-27 14:55:22 +0100

    function to create a .sql file

---
+++ load/create_sql_file.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+# create a .sql file from using .db file
+def create_dot_sql_file(database_name: str, dot_sql_file: str):
+    conn = sqlite3.connect(database_name)
+    cursor = conn.cursor()
+
+
+    with open(dot_sql_file, 'a') as f:
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+
+        for table in tables:
+            table_name = table[0]
+            cursor.execute(f"SELECT sql FROM sqlite_master WHERE type='table' AND name='{table_name}';")
+            create_table_sql = cursor.fetchone()[0]
+            f.write(f"{create_table_sql};\n")
+
+            cursor.execute(f"SELECT * FROM {table_name};")
+            rows = cursor.fetchall()
+            for row in rows:
+                insert_sql = f"INSERT INTO {table_name} VALUES {row};"
+                f.write(f"{insert_sql}\n")
+
+    conn.close()
